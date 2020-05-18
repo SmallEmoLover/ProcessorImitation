a8e3e687a259ba196d97ab0e13121cce8f101c3d
SmallEmoLover <missingtext@yandex.ru>
2020-05-18 20:29:45 +0300

    Реализация класса процессора

---
+++ src/Processor.cpp
@@ -0,0 +1,38 @@
+#include "Processor.h"
+
+/*
+    Кулаков Д.С. :с
+    ИВТ-13БО
+    Лабораторная по имитированию работы процессора
+    Реализация класса процессора
+*/
+
+Processor::Processor()
+{
+    tasks = new Stack();
+    isFree = true;
+}
+
+void Processor::newTask(Task task)
+{
+    if(isFree == true)
+    {
+        currentTask = task;
+        currentTask.execute();
+        isFree == false;
+    }
+    else
+        tasks->push(task);    
+}
+
+void Processor::onStopTask()
+{
+    if (tasks->isEmpty())
+        isFree == true;
+    else
+    {
+        currentTask = tasks->pop();
+        currentTask.execute();
+    }
+}
+
--- src/Processor.h
+++ src/Processor.h
@@ -3,13 +3,23 @@
 #include "Stack.h"
 #include "Task.h"
 
+/*
+    Кулаков Д.С. :с
+    ИВТ-13БО
+    Лабораторная по имитированию работы процессора
+    Класс процессора
+*/
+
 class Processor
 {
     private:
-        Stack tasks;
+        Stack *tasks;
         bool isFree;
-
+        Task currentTask;
+        //При остановке задачи
+        void onStopTask();
     public:
+        //Добавление новой задачи
         void newTask(Task task);
         Processor();
 };
