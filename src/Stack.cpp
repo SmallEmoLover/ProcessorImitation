576e0cb31ba76d876a478106538f688d81ab10c7
SmallEmoLover <missingtext@yandex.ru>
2020-05-18 19:51:24 +0300

    Реализация класса стеков

---
+++ src/Stack.cpp
@@ -0,0 +1,42 @@
+#include "Stack.h"
+
+/*
+    Кулаков Д.С. :с
+    ИВТ-13БО
+    Лабораторная по имитированию работы процессора
+    Реализация класса стека
+*/
+
+Stack::Stack()
+{
+    first == nullptr;
+}
+
+void Stack::push(Task data)
+{
+    if(Stack::isEmpty())
+        first = new Node(data);
+    else
+    {
+        Node* tmp = first;
+        first = new Node(data);
+        first->setNext(tmp);
+    }
+}
+
+Task Stack::pop()
+{
+    Task result = first->getData();
+    Node *cur = first;
+    first = cur->getNext();
+    delete cur;
+    return result;
+}
+
+bool Stack::isEmpty()
+{
+    if (first == nullptr)
+        return true;
+    else
+        return false;
+}
--- src/Stack.h
+++ src/Stack.h
@@ -3,13 +3,24 @@
 #include "Node.h"
 #include "Task.h"
 
+/*
+    Кулаков Д.С. :с
+    ИВТ-13БО
+    Лабораторная по имитированию работы процессора
+    Класс стека
+*/
+
 class Stack
 {
     private:
         Node* first;
-        
+
     public:
+        //добавить элемент
         void push(Task data);
+        //взять элемент
         Task pop();
+        //пуст ли стек
         bool isEmpty();
+        Stack();
 };
